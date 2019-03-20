c284f5c91f95c3ae7fb0f108e2731cf5c9f265e5
adrpre14 <adrpre14@htl-shkoder.com>
2019-03-20 15:56:03 +0100

    Kommentare in OurTable Klasse.

--- Projekt4ay/src/OurTable.java
+++ Projekt4ay/src/OurTable.java
@@ -11,13 +11,16 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author Adriano Preçaj
  */
+//Wir erstellen eine Klasse die die DefaultTableModel vererbt, damit wir UNSERE Model der Tabelle machen 
 public class OurTable extends DefaultTableModel{
     int columnDisabled=0;
+    //Wir erstellen eine Konstruktor, die die DefaultTableModel Konstruktor aufruft und aendert die variable columnDisabled
     public OurTable(int columnDisabled){
         super();
         this.columnDisabled=columnDisabled;
     }
     @Override
+    //Macht die Spalte nicht bearbeitbar
     public boolean isCellEditable(int row, int column){
         if(column==columnDisabled){
             return false;
